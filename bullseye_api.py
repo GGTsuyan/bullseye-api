f9a5b7ee1eaac0731d1a8fb76f2f51845dc08e42
VLV999 <180421608+VLV999@users.noreply.github.com>
2025-09-09 01:07:35 +0800

    iou_threshold

--- bullseye_api.py
+++ bullseye_api.py
@@ -223,7 +223,7 @@ def run_detector(image_bgr, debug=False):
             boxes=boxes,
             scores=scores,
             max_output_size=MAX_DARTS,
-            iou_threshold=0.5,
+            iou_threshold=0.4,
             score_threshold=CONFIDENCE_THRESHOLD
         ).numpy()
 
